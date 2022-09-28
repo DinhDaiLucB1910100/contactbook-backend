67776c9b36d3f2b46b2786bc2b428d81e30e2570
luckont <luckont4545@gmail.com>
2022-09-29 01:21:47 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import app from "./app.js";
 import config from "./app/config/index.js";
+import MongoDB from "./app/untils/mongodb.until.js";
 
-const PORT = config.app.port;
+async function startServer(){
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Connect to the database!");
 
-app.listen(PORT, () => {
-    console.log(`Server is running port ${PORT}.`);
-});
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Cannot connect to the dahtabase!", error);
+        process.exit();
+    }
+}
 
+startServer();
